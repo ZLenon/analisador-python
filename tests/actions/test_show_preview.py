d327401d01e614418b71fde30ecc0b3965804c1f
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-23 16:51:51 -0300

    Test: mudando a saida da função show_preview

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -3,10 +3,8 @@ from pro_filer.actions.main_actions import show_preview
 # =====================MOCK=======================
 contexto_falha = {"all_files": [], "all_dirs": []}
 contexto_sucesso = {
-    "all_files": [
-        "src/middleware/isEmail.py",
-    ],
-    "all_dirs": ["src", "src/middleware"],
+    "all_files": ["src/__init__.py", "src/app.py", "src/utils/__init__.py"],
+    "all_dirs": ["src", "src/utils"],
 }
 
 
@@ -16,9 +14,9 @@ def test_show_preview_sucess_case(capsys):
     rota = capsys.readouterr()
     assert (
         rota.out
-        == """Found 1 files and 2 directories
-First 5 files: ['src/middleware/isEmail.py']
-First 5 directories: ['src', 'src/middleware']
+        == """Found 3 files and 2 directories
+First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']
+First 5 directories: ['src', 'src/utils']
 """
     )
 
