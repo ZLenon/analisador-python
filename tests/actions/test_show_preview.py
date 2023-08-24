f68eae6f2c1ad127b7e9483d92406882ccde4435
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-23 22:28:47 -0300

    Test: função show_preview ajustando testes

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -3,43 +3,35 @@ from pro_filer.actions.main_actions import show_preview
 # =====================MOCK=======================
 contexto_falha = {"all_files": [], "all_dirs": []}
 saida_falha = "Found 0 files and 0 directories\n"
-contexto_sucesso_arquivos = {
+contexto_sucesso = {
     "all_files": [
         "index.js",
         "index.css",
         "index.html",
         "index.ts",
         "index.js",
+        "index.py",
     ],
-    "all_dirs": ["src"],
-}
-contexto_sucesso_diretorios = {
-    "all_files": ["controller.ts"],
-    "all_dirs": ["controller", "model", "service", "middleware", "utils"],
+    "all_dirs": [
+        "src",
+        "controller",
+        "model",
+        "service",
+        "middleware",
+        "utils",
+    ],
 }
 
 
 # ==============TESTES SUCESSO===================
 def test_show_preview_sucess_case_files(capsys):
-    show_preview(contexto_sucesso_arquivos)
+    show_preview(contexto_sucesso)
     rota = capsys.readouterr()
     assert (
         rota.out
-        == """Found 5 files and 1 directories
+        == """Found 6 files and 6 directories
 First 5 files: ['index.js', 'index.css', 'index.html', 'index.ts', 'index.js']
-First 5 directories: ['src']
-"""
-    )
-
-
-def test_show_preview_sucess_case_directories(capsys):
-    show_preview(contexto_sucesso_diretorios)
-    rota = capsys.readouterr()
-    assert (
-        rota.out
-        == """Found 1 files and 5 directories
-First 5 files: ['controller.ts']
-First 5 directories: ['controller', 'model', 'service', 'middleware', 'utils']
+First 5 directories: ['src', 'controller', 'model', 'service', 'middleware']
 """
     )
 
