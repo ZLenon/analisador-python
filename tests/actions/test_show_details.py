6fc703e9618903a141c762055bba0f7e9433207b
Lenon Nascimento <lenoninformatica@gmail.com>
2023-08-24 16:14:06 -0300

    Test: Função show_details caso de sucesso e de falha

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,1 +1,26 @@
-from pro_filer.actions.main_actions import show_details  # NOQA
+from pro_filer.actions.main_actions import show_details
+
+contexto_sucesso = {
+    "base_path": "images/pro-filer-preview.gif",
+}
+contexto_falha = {"base_path": "/src/utils/????"}
+
+
+def test_show_details(capsys, tmp_path):
+    show_details(contexto_sucesso)
+    captured = capsys.readouterr()
+    assert (
+        captured.out
+        == """File name: pro-filer-preview.gif
+File size in bytes: 270824
+File type: file
+File extension: .gif
+Last modified date: 2023-08-22
+"""
+    )
+
+
+def test_show_details_file(capsys):
+    show_details(contexto_falha)
+    captured = capsys.readouterr()
+    assert captured.out == "File '????' does not exist\n"
